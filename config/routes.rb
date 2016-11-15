ab66006b4fcfd776f5c9c6eb008867238861dace
Lino <me@awesomelionel.com>
2016-11-15 08:06:14 +0800

    Add shout render

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'homes/show'
 
   root to: "homes#show", via: :get
+  resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :shouts, only: [:create]
 end
