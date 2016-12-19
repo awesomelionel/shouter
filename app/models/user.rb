f8225d2d36c297f2cc273504d0dc1b529067c57a
Lino <me@awesomelionel.com>
2016-12-19 20:21:21 +0800

    Add ability to follow users
    We give the ability for users to follow each other through the
    association of the FollowingRelationships model.
    This model allows users to follow and be followed by other users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+
   has_many :shouts
+
+  # Used in order to follow users
+  has_many :followed_user_relationships,
+    foreign_key: :follower_id,
+    class_name: 'FollowingRelationship'
+  has_many :followed_users, through: :followed_user_relationships
+
+  # Used to show how many followers you have
+  has_many :follower_relationships,
+    foreign_key: :followed_user_id, 
+    class_name: 'FollowingRelationship'
+  has_many :followers, through: :follower_relationships
 end
